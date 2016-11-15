a04c76f9c7562758ffa9d83f4e6a64b1b88ecf08
Zoltan Nagy <abesto@abesto.net>
2016-11-15 16:18:21 +0100

    Limit to 30 FPS

--- src/vizceral.js
+++ src/vizceral.js
@@ -696,7 +696,10 @@ class Vizceral extends EventEmitter {
   }
 
   animate (time) {
-    requestAnimationFrame(this.animate.bind(this));
+    var that = this;
+    setTimeout(function() {
+      requestAnimationFrame(that.animate.bind(that));
+    }, 1000 / 30);
     this.render(time);
   }
 
